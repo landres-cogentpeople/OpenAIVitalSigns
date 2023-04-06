019257a8d7fdd184f2867e57e63d0cde1674beb6
landres-cogentpeople <landres@cogentpeople.com>
2023-04-06 15:30:29 -0400

    Added comments for GitHub gist.

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import json
 import os
 
 from flask import Flask
@@ -31,17 +30,25 @@ def create_app(test_config=None):
     except OSError:
         pass
 
+    # Load the API_KEY.  The line below only sets a temporary API_KEY
     openai.api_key = "<API_KEY>"
+
+    # The actual API_KEY is loaded from the "apiKey.txt" file in the line below.
     with open("apiKey.txt", "r") as filein:
         fileline = filein.readline()
         openai.api_key = fileline.strip()
 
+    # The following function serves static files under the "/static" base URL
     @app.route("/static/<path:path>")
     def sendStaticFile(path):
         return send_from_directory("static", path)
 
+    # The following function serves static files under the "/get_json_translation"
+    # URL endpoint.
     @app.route("/get_json_translation", methods=["POST"])
     def getJSONTranslation():
+
+        # The prompt, along with context data, is stored in the requestText python variable.
         requestText = """
 Patient Note:
 
@@ -56,17 +63,28 @@ respiratory_rate: respiratory rate in breaths per minute
 height: height in centimeters
 weight: weight in kilograms
 """.format(request.form["patient_note_text"])
+
+        # The next line prints the prompt that will be sent to OpenAI for debugging purposes.
+        print(requestText)
+
+        # This makes the call to OpenAI to be processed using the "text-davinci-003" model
         jsonString = openai.Completion.create(
             model="text-davinci-003",
             prompt=requestText,
             max_tokens=300
         )
+
+        # The next line prints the returned string from OpenAI for debugging purposes.
         print(jsonString)
+
+        # This returns the result as a jsonString
         return jsonString
 
+    # This returns the entire app as a result of the create_app() method.
     return app
 
 
+# Main call to start the application.
 if __name__ == "__main__":
     app = create_app()
     app.run(debug=True, port=5000, host="0.0.0.0")
